0f4db507edb42651e7b04563ca19c44f6a469bcd
Максимилиан <pavlovmaksimilian1@gmail.com>
2022-10-12 19:11:38 +0300

    Setup.py added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Drill planner',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Maksimilian Pavlov',
+    author_email='pavlovmaksimilian1@gmail.com',
+    description='A module for autoplanning and scheduling of drilling operations'
+)
